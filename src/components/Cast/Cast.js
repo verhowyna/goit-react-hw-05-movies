30c9202ab93efd841601107202ff51a021e55160
verhowyna <verhowyna@ukr.net>
2023-12-27 15:02:37 +0200

    cast-reviiews

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -1,3 +1,58 @@
+import { Loader } from 'components/Loader/Loader';
+import { getCast } from 'components/api';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useParams } from 'react-router-dom';
+
 export const Cast = () => {
-  return <div>Cast</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [cast, setCast] = useState([]);
+
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    const fetchCast = async () => {
+      setIsLoading(true);
+      try {
+        const { cast } = await getCast(movieId);
+        setCast([...cast]);
+      } catch (error) {
+        toast.error('Oops! Something went wrong. Please try again later.');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchCast();
+  }, [movieId]);
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      {cast.length > 0 && (
+        <div>
+          <ul>
+            {cast.map(actor => {
+              const { profile_path, name, character } = actor;
+              const BASE_URL = 'https://image.tmdb.org/t/p/w200';
+              const photo = BASE_URL + profile_path;
+              return (
+                <li key={name}>
+                  {profile_path ? (
+                    <img src={photo} alt={name} />
+                  ) : (
+                    <div>NoPoster</div>
+                  )}
+                  <div>
+                    <p>{name}</p>
+                    <p>Character: {character}</p>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 };
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,3 +1,51 @@
+import { Loader } from 'components/Loader/Loader';
+import { getReviews } from 'components/api';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import { useParams } from 'react-router-dom';
+
 export const Reviews = () => {
-  return <div>Reviews</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [reviews, setReviews] = useState([]);
+
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      setIsLoading(true);
+      try {
+        const { results } = await getReviews(movieId);
+        setReviews([...results]);
+      } catch (error) {
+        toast.error('Oops! Something went wrong. Please try again later.');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchReviews();
+  }, [movieId]);
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      {reviews.length > 0 && (
+        <div>
+          <ul>
+            {reviews.map(review => {
+              const { author, content } = review;
+              return (
+                <li key={author}>
+                  <h3>{author}</h3>
+                  <p>{content}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+      {reviews.length === 0 && !isLoading && (
+        <div>We don't have any reviews for this movie yet.</div>
+      )}
+    </>
+  );
 };
