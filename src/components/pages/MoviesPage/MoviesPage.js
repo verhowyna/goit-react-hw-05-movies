ecfd2acc4dad8512dae042777405fdc5cda7187f
verhowyna <verhowyna@ukr.net>
2023-12-27 18:40:16 +0200

    not styled

--- src/components/pages/MoviesPage/MoviesPage.js
+++ src/components/pages/MoviesPage/MoviesPage.js
@@ -2,12 +2,12 @@ import { Loader } from 'components/Loader/Loader';
 import { searchMovies } from 'components/api';
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
+import { IoIosPhotos, IoMdSearch } from 'react-icons/io';
 import { NavLink, useLocation, useSearchParams } from 'react-router-dom';
 
 export default function MoviesPage() {
   const [isLoading, setIsLoading] = useState(false);
   const [movies, setMovies] = useState([]);
-
   const [params, setParams] = useSearchParams();
   const searchedMovie = params.get('movie') ?? '';
 
@@ -53,35 +53,50 @@ export default function MoviesPage() {
           placeholder="Search movie"
           name="movie"
         />
-        <button type="button">Search</button>
+        <button type="submit">
+          <IoMdSearch size="24" />
+        </button>
       </form>
       {isLoading && <Loader />}
-      <div>
-        <ul>
-          {movies.length > 0 &&
-            movies.map(movie => {
-              const { id, title, poster_path } = movie;
-              const BASE_URL = 'https://image.tmdb.org/t/p/w200';
-              const photo = BASE_URL + poster_path;
 
-              return (
-                <li key={id}>
-                  <NavLink to={`${id}`} state={{ from: location }}>
-                    {poster_path ? (
-                      <img src={photo} alt={title} />
-                    ) : (
-                      <div>No Poster</div>
-                    )}
-                    <h3>{title}</h3>
-                  </NavLink>
-                </li>
-              );
-            })}
-        </ul>
-      </div>
-      {movies.length === 0 && !isLoading && searchedMovie && (
-        <div>Movie "{searchedMovie}" not found.</div>
+      {movies.length > 0 && !isLoading && (
+        <div>
+          <ul>
+            {movies.length > 0 &&
+              movies.map(movie => {
+                const { id, title, poster_path } = movie;
+                const BASE_URL = 'https://image.tmdb.org/t/p/w200';
+                const photo = BASE_URL + poster_path;
+
+                return (
+                  <li key={id}>
+                    <NavLink to={`${id}`} state={{ from: location }}>
+                      {poster_path ? (
+                        <img src={photo} alt={title} />
+                      ) : (
+                        <IoIosPhotos
+                          style={{
+                            width: '200px',
+                            height: '200px',
+                            color: '#8080803b',
+                          }}
+                        />
+                      )}
+                      <h3>{title}</h3>
+                    </NavLink>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
       )}
+
+      {movies.length === 0 &&
+        !isLoading &&
+        searchedMovie &&
+        toast.error(
+          'Sorry, there are no movies matching your search query. Please try again.'
+        )}
     </>
   );
 }
